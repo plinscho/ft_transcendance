651e8b1516907be5d9dfcfda65a0b821b410e9f9
Pol Ramos <119877499+pramos-m@users.noreply.github.com>
2025-02-25 17:32:17 +0100

    El menú sigue con normalidad con esc, back, y cambiando el menú

--- srcs/frontend/static/views.js
+++ srcs/frontend/static/views.js
@@ -6,10 +6,10 @@ const D = document;
 
 // Determine the current view
 let currentView = () => {
-    if (state.loading) return View.LOADING;
-    if (state.error) return View.ERROR;
-    if (!state.authenticated) return View.NEEDS_AUTH;
-    return View.OK;
+	if (state.loading) return View.LOADING;
+	if (state.error) return View.ERROR;
+	if (!state.authenticated) return View.NEEDS_AUTH;
+	return View.OK;
 };
 
 // DOM node references
@@ -21,25 +21,26 @@ let $viewReady = D.getElementById('view-ready');
 let $currentView = $viewLoading;
 
 let $$viewNodes = [
-    $viewLoading,
-    $viewReady,
-    $viewNeedsLogin,
-    $viewFailure,
+	$viewLoading,
+	$viewReady,
+	$viewNeedsLogin,
+	$viewFailure,
 ];
 
 // DOM updates
-export const updateView = async () => {
+export const updateView = () => {
     console.log("Updating view with state:", state);
     let $nextView = $$viewNodes[currentView()];
     if ($nextView === $currentView) return;
     $currentView.classList.add('invisible');
     $nextView.classList.remove('invisible');
     $currentView = $nextView;
-    await updateUITexts();
+    updateUITexts();
 };
 
-export const updateInitialView = async () => {
+
+export const  updateInitialView = () => {
     console.log(state);
-    await updateView();
+	updateView();
     startGame();
 };
