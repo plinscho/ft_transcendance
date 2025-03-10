0437ee5e3f0d7e83c7608dfcba9e95e88843df85
Bifrost <nkeyani-@student.42barcelona.com>
2025-03-10 11:14:59 +0100

    camera fix for local

--- srcs/frontend/static/Pong/Lighting.js
+++ srcs/frontend/static/Pong/Lighting.js
@@ -2,21 +2,21 @@ import * as THREE from 'three';
 
 export class Lighting {
     constructor(scene) {
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
         scene.add(ambientLight);
 
-        const pointLight = new THREE.PointLight(0xffffff, 20, 1500, 0.1);
-        pointLight.position.set(0, 100, 0);
+        const pointLight = new THREE.PointLight(0xffffff, 20, 1500, 0);
+        pointLight.position.set(0, 200, 0);
         pointLight.castShadow = true;
         pointLight.shadow.mapSize.set(2048, 2048);
         scene.add(pointLight);
 
         const spotLight = new THREE.SpotLight(0xffffff, 30);
-        spotLight.position.set(0, 0, 0);
+        spotLight.position.set(0, 200, 0);
         spotLight.angle = Math.PI / 4;
         spotLight.penumbra = 0.5;
-        spotLight.decay = 1;
-        spotLight.distance = 1000;
+        spotLight.decay = 0;
+        spotLight.distance = 10000;
         spotLight.castShadow = true;
         spotLight.shadow.mapSize.set(2048, 2048);
         scene.add(spotLight);
--- srcs/frontend/static/Pong/Pong.js
+++ srcs/frontend/static/Pong/Pong.js
@@ -102,7 +102,7 @@ export class Pong {
 	}
 	
 	updateLocalCoopCamera() {
-		this.cameraManager.updateCamera(this.localcoopCamera, this.ball);
+		this.cameraManager.updateCamera(this.localcoopCamera);
 	}
 	
 
