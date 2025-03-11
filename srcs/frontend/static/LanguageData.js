cd6b31c97ecb1d655a1045b43d7acbfd1185be8b
Pol Ramos <119877499+pramos-m@users.noreply.github.com>
2025-03-11 19:09:26 +0100

    lang added to tournaments

--- srcs/frontend/static/LanguageData.js
+++ srcs/frontend/static/LanguageData.js
@@ -1,156 +1,204 @@
 const languages = {
-    en: {
-      loading: "Loading...",
-      error: {
-        title: "Failed to load!",
-        message: "An unexpected error occurred. Please try again later.",
-        back: "BACK TO LOGIN"
-      },
-      login: {
-        title: "Login",
-        email: "Email address",
-        password: "Password",
-        submit: "Sign in",
-        registerPrompt: "Not a member?",
-        registerLink: "Register"
-      },
-      twoFactor: {
-        title: "2FA",
-        code: "2FA Code",
-        verify: "Verify"
-      },
-      register: {
-        title: "Register",
-        username: "Username",
-        email: "Email address",
-        password: "Password",
-        submit: "Register",
-        loginPrompt: "Already have an account?",
-        loginLink: "Login"
-      },
-      menu: {
-        play: "Play",
-        localcoop: "Local Co-op",
-        multiplayer: "Multiplayer",
-        tournament: "Tournament",
-        languages: "Languages",
-        logout: "Logout"
-      }
+  en: {
+    loading: "Loading...",
+    error: {
+      title: "Failed to load!",
+      message: "An unexpected error occurred. Please try again later.",
+      back: "BACK TO LOGIN"
     },
-    es: {
-      loading: "Cargando...",
-      error: {
-        title: "¡Error al cargar!",
-        message: "Ocurrio un error inesperado. Por favor, inténtelo de nuevo mas tarde.",
-        back: "VOLVER AL INICIO"
-      },
-      login: {
-        title: "Iniciar sesion",
-        email: "Correo electrónico",
-        password: "Contraseña",
-        submit: "Iniciar sesion",
-        registerPrompt: "¿No eres miembro?",
-        registerLink: "Registrarse"
-      },
-      twoFactor: {
-        title: "2FA",
-        code: "Código 2FA",
-        verify: "Verificar"
-      },
-      register: {
-        title: "Registrarse",
-        username: "Nombre de usuario",
-        email: "Correo electrónico",
-        password: "Contraseña",
-        submit: "Registrarse",
-        loginPrompt: "¿Ya tienes una cuenta?",
-        loginLink: "Iniciar sesion"
-      },
-      menu: {
-        play: "Jugar",
-        localcoop: "Multijugador local",
-        multiplayer: "Multijugador",
-        tournament: "Torneo",
-        languages: "Idiomas",
-        logout: "Cerrar sesion"
-      }
+    login: {
+      title: "Login",
+      email: "Email address",
+      password: "Password",
+      submit: "Sign in",
+      registerPrompt: "Not a member?",
+      registerLink: "Register"
     },
-    it: {
-      loading: "Caricamento...",
-      error: {
-        title: "Caricamento fallito!",
-        message: "Si è verificato un errore imprevisto. Per favore riprova più tardi.",
-        back: "TORNA AL LOGIN"
-      },
-      login: {
-        title: "Accedi",
-        email: "Indirizzo email",
-        password: "Password",
-        submit: "Accedi",
-        registerPrompt: "Non sei un membro?",
-        registerLink: "Registrati"
-      },
-      twoFactor: {
-        title: "2FA",
-        code: "Codice 2FA",
-        verify: "Verifica"
-      },
-      register: {
-        title: "Registrati",
-        username: "Nome utente",
-        email: "Indirizzo email",
-        password: "Password",
-        submit: "Registrati",
-        loginPrompt: "Hai già un account?",
-        loginLink: "Accedi"
-      },
-      menu: {
-        play: "Giocare",
-        localcoop: "Multigiocatore locale",
-        multiplayer: "Multigiocatore",
-        tournament: "Torneo",
-        languages: "Lingue",
-        logout: "Disconnettersi"
-      }
+    twoFactor: {
+      title: "2FA",
+      code: "2FA Code",
+      verify: "Verify"
     },
-    fr: {
-      loading: "Chargement...",
-      error: {
-        title: "Échec du chargement!",
-        message: "Une erreur inattendue s'est produite. Veuillez réessayer plus tard.",
-        back: "RETOUR À LA CONNEXION"
-      },
-      login: {
-        title: "Connexion",
-        email: "Adresse e-mail",
-        password: "Mot de passe",
-        submit: "Se connecter",
-        registerPrompt: "Pas encore membre?",
-        registerLink: "S'inscrire"
-      },
-      twoFactor: {
-        title: "2FA",
-        code: "Code 2FA",
-        verify: "Vérifier"
-      },
-      register: {
-        title: "S'inscrire",
-        username: "Nom d'utilisateur",
-        email: "Adresse e-mail",
-        password: "Mot de passe",
-        submit: "S'inscrire",
-        loginPrompt: "Vous avez déjà un compte?",
-        loginLink: "Se connecter"
-      },
-      menu: {
-        play: "Jouer",
-        localcoop: "Multijoueur local",
-        multiplayer: "Multijoueur",
-        tournament: "Tournoi",
-        languages: "Langues",
-        logout: "Se déconnecter"
-      }
+    register: {
+      title: "Register",
+      username: "Username",
+      email: "Email address",
+      password: "Password",
+      submit: "Register",
+      loginPrompt: "Already have an account?",
+      loginLink: "Login"
+    },
+    menu: {
+      play: "Play",
+      localcoop: "Local Co-op",
+      multiplayer: "Multiplayer",
+      tournament: "Tournament",
+      languages: "Languages",
+      logout: "Logout"
+    },
+    waitingRoom: {
+      waitingText: "Waiting for other player...",
+      escToLeave: "ESC TO LEAVE QUEUE",
+      selectLanguage: "Select Language"
+    },
+    tournament: {
+      player: "Player",
+      placeholder: "alias",
+      start: "Start!",
+      nickEmpty: "Watch out! Nickname can't be empty",
+      duplicateNick: "Duplicate nickname"
     }
-  };
+  },
+  es: {
+    loading: "Cargando...",
+    error: {
+      title: "¡Error al cargar!",
+      message: "Ocurrio un error inesperado. Por favor, inténtelo de nuevo mas tarde.",
+      back: "VOLVER AL INICIO"
+    },
+    login: {
+      title: "Iniciar sesion",
+      email: "Correo electrónico",
+      password: "Contraseña",
+      submit: "Iniciar sesion",
+      registerPrompt: "¿No eres miembro?",
+      registerLink: "Registrarse"
+    },
+    twoFactor: {
+      title: "2FA",
+      code: "Código 2FA",
+      verify: "Verificar"
+    },
+    register: {
+      title: "Registrarse",
+      username: "Nombre de usuario",
+      email: "Correo electrónico",
+      password: "Contraseña",
+      submit: "Registrarse",
+      loginPrompt: "¿Ya tienes una cuenta?",
+      loginLink: "Iniciar sesion"
+    },
+    menu: {
+      play: "Jugar",
+      localcoop: "Multijugador local",
+      multiplayer: "Multijugador",
+      tournament: "Torneo",
+      languages: "Idiomas",
+      logout: "Cerrar sesion"
+    },
+    waitingRoom: {
+      waitingText: "Esperando a otro jugador...",
+      escToLeave: "ESC PARA SALIR DE LA COLA",
+      selectLanguage: "Seleccionar Idioma"
+    },
+    tournament: {
+      player: "Jugador",
+      placeholder: "alias",
+      start: "¡Comenzar!",
+      nickEmpty: "¡Cuidado! El apodo no puede estar vacío",
+      duplicateNick: "Apodo duplicado"
+    }
+  },
+  it: {
+    loading: "Caricamento...",
+    error: {
+      title: "Caricamento fallito!",
+      message: "Si è verificato un errore imprevisto. Per favore riprova più tardi.",
+      back: "TORNA AL LOGIN"
+    },
+    login: {
+      title: "Accedi",
+      email: "Indirizzo email",
+      password: "Password",
+      submit: "Accedi",
+      registerPrompt: "Non sei un membro?",
+      registerLink: "Registrati"
+    },
+    twoFactor: {
+      title: "2FA",
+      code: "Codice 2FA",
+      verify: "Verifica"
+    },
+    register: {
+      title: "Registrati",
+      username: "Nome utente",
+      email: "Indirizzo email",
+      password: "Password",
+      submit: "Registrati",
+      loginPrompt: "Hai già un account?",
+      loginLink: "Accedi"
+    },
+    menu: {
+      play: "Giocare",
+      localcoop: "Multigiocatore locale",
+      multiplayer: "Multigiocatore",
+      tournament: "Torneo",
+      languages: "Lingue",
+      logout: "Disconnettersi"
+    },
+    waitingRoom: {
+      waitingText: "In attesa di un altro giocatore...",
+      escToLeave: "ESC PER USCIRE DALLA CODA",
+      selectLanguage: "Seleziona Lingua"
+    },
+    tournament: {
+      player: "Giocatore",
+      placeholder: "alias",
+      start: "Inizia!",
+      nickEmpty: "Attenzione! Il nickname non può essere vuoto",
+      duplicateNick: "Nickname duplicato"
+    }
+  },
+  fr: {
+    loading: "Chargement...",
+    error: {
+      title: "Échec du chargement!",
+      message: "Une erreur inattendue s'est produite. Veuillez réessayer plus tard.",
+      back: "RETOUR À LA CONNEXION"
+    },
+    login: {
+      title: "Connexion",
+      email: "Adresse e-mail",
+      password: "Mot de passe",
+      submit: "Se connecter",
+      registerPrompt: "Pas encore membre?",
+      registerLink: "S'inscrire"
+    },
+    twoFactor: {
+      title: "2FA",
+      code: "Code 2FA",
+      verify: "Vérifier"
+    },
+    register: {
+      title: "S'inscrire",
+      username: "Nom d'utilisateur",
+      email: "Adresse e-mail",
+      password: "Mot de passe",
+      submit: "S'inscrire",
+      loginPrompt: "Vous avez déjà un compte?",
+      loginLink: "Se connecter"
+    },
+    menu: {
+      play: "Jouer",
+      localcoop: "Multijoueur local",
+      multiplayer: "Multijoueur",
+      tournament: "Tournoi",
+      languages: "Langues",
+      logout: "Se déconnecter"
+    },
+    waitingRoom: {
+      waitingText: "En attente d'un autre joueur...",
+      escToLeave: "ESC POUR QUITTER LA FILE",
+      selectLanguage: "Sélectionner la Langue"
+    },
+    tournament: {
+      player: "Joueur",
+      placeholder: "alias",
+      start: "Commencer!",
+      nickEmpty: "Attention! Le surnom ne peut pas être vide",
+      duplicateNick: "Surnom en double"
+    }
+  }
+};
 
 export { languages };
--- srcs/frontend/static/WaitingRoom.js
+++ srcs/frontend/static/WaitingRoom.js
@@ -1,8 +1,175 @@
+// import * as THREE from 'three';
+// import { Text3D } from './Text3D.js';
+// import { SetNickEl } from './WaitingRoomNickEl.js';
+// import { TournamentManager } from './TournamentManager.js';
+// import { PongBackground } from './Pong/PongBackground.js';
+
+// export class WaitingRoom {
+// 	constructor(state, network) {
+// 		this.scene = new THREE.Scene();
+// 		this.camera = this.createCamera();
+// 		new PongBackground(this.scene);
+// 		this.network = network;
+// 		this.isWaiting = true;
+// 		this.active = true;
+// 		this.state = state;
+// 		this.buttons = [];
+// 		this.SetNickEl;
+// 		this.isTournament = this.state.isTournament;
+// 		this.boundEscapeHandler = this.escapeHandler.bind(this);
+// 		this.setUpKeyboard();
+// 		this.state.resize();
+// 		this.createWaitingRoom();
+// 	}
+
+// 	createTournamentScreen() {
+// 		const backToMenu = new Text3D(
+// 			"ESC TO LEAVE QUEUE",
+// 			{ x: -2, y: 1, z: 0 },
+// 			0xffffff,
+// 			0.15,
+// 			0,
+// 			() => { if (this.active) { this.backToMenu(); }}
+// 		);
+
+// 		backToMenu.createText((textMesh) => {
+// 			textMesh.userData.onClick = backToMenu.onClick;
+// 			this.scene.add(textMesh);
+// 			this.buttons.push(textMesh);
+// 		});
+
+// 		this.SetNickEl = document.createElement('set-nick-el');
+// 		this.SetNickEl.setState(this);
+// 		document.body.appendChild(this.SetNickEl);
+// 		//setNickElement.setNetwork(networkInstance);
+// 	}
+
+// 	createMultiplayer() {
+// 		const waitingText = new Text3D(
+// 			"Waiting for other player...",
+// 			{ x: -4, y: 0, z: 0 },
+// 			0xffffff,
+// 			0.4,
+// 			0,
+// 			() => {}
+// 		);
+
+// 		waitingText.createText((textMesh) => {
+// 			this.scene.add(textMesh);
+// 		});
+
+// 		const backToMenu = new Text3D(
+// 			"ESC TO LEAVE QUEUE",
+// 			{ x: -2, y: 1, z: 0 },
+// 			0xffffff,
+// 			0.2,
+// 			0,
+// 			() => { if (this.active) { this.backToMenu(); }}
+// 		);
+
+// 		backToMenu.createText((textMesh) => {
+// 			textMesh.userData.onClick = backToMenu.onClick;
+// 			this.scene.add(textMesh);
+// 			this.buttons.push(textMesh);
+// 		});
+// 		this.network.connect();
+// 		this.network.onMessage((response) => this.handleServerMessage(response));
+// 	}
+
+// 	createWaitingRoom() {
+// 		console.log(this.isTournament);
+// 		if (!this.isTournament) {
+// 			this.createMultiplayer();
+// 		}
+// 		else {
+// 			this.createTournamentScreen();
+// 		}
+// 	}
+
+// 	launchTournament() {
+// 		this.state.tournamentManager = new TournamentManager(this.state, this.SetNickEl.getNickNames());
+// 		this.state.loadScene(this.state.states.TOURNAMENTS);
+// 	}
+	
+// 	handleServerMessage(data) {
+//         console.log("Received message from server: ", data);
+//         if (data.type === "PLAYER_ONE") this.state.player1 = true;
+//         if (data.type === "PLAYER_TWO") this.state.player2 = true;
+//         if (data.type === "START_GAME") {
+//             console.log("Match found! Starting game...");
+//             this.isWaiting = false;
+//             if (this.SetNickEl)
+//                 this.SetNickEl.remove();
+//             this.startMultiplayerGame();
+//         }
+    
+//     }
+
+//     startMultiplayerGame() {
+//         console.log("Starting multiplayer Pong...");
+//         this.state.loadScene(this.state.states.MULTIPLAYER);
+//     }
+
+//     createCamera() {
+//         const aspect = window.innerWidth / window.innerHeight;
+//         const frustumSize = 5;
+
+//         const camera = new THREE.OrthographicCamera(
+//             -frustumSize * aspect,
+//             frustumSize * aspect,
+//             frustumSize,
+//             -frustumSize,
+//             0.1, 100
+//         );
+
+//         camera.position.set(0, 0, 10);
+//         camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+//         return camera;
+//     }
+
+//     setUpKeyboard() {
+//         window.addEventListener('keydown', this.boundEscapeHandler);
+//     }
+
+//     escapeHandler(event) {
+//         if (event.key === 'Escape' && this.isWaiting && this.active) {
+//             this.backToMenu();
+//         }
+//     }
+
+//     backToMenu() {
+// 		if (this.isTournament) {
+// 			this.SetNickEl.remove();
+// 			this.state.isTournament = false;
+//         } else {
+// 			this.network.sendData({ type: "QUIT" });
+// 			this.network.disconnect();
+// 		}
+// 		window.removeEventListener('keydown', this.boundEscapeHandler);
+// 		this.active = false;
+//         this.state.loadScene(this.state.states.MENU);
+//     }
+
+//     cleanup() {
+//         window.removeEventListener('keydown', this.boundEscapeHandler);
+//     }
+
+//     getScene() {
+//         return this.scene;
+//     }
+
+//     getCamera() {
+//         return this.camera;
+//     }
+// }
+
 import * as THREE from 'three';
 import { Text3D } from './Text3D.js';
 import { SetNickEl } from './WaitingRoomNickEl.js';
 import { TournamentManager } from './TournamentManager.js';
 import { PongBackground } from './Pong/PongBackground.js';
+import { lang } from './Languages.js';
 
 export class WaitingRoom {
 	constructor(state, network) {
@@ -20,11 +187,15 @@ export class WaitingRoom {
 		this.setUpKeyboard();
 		this.state.resize();
 		this.createWaitingRoom();
+                
+                // Añadir un listener para cambios de idioma
+                this.languageChangedHandler = this.handleLanguageChange.bind(this);
+                window.addEventListener('languageChanged', this.languageChangedHandler);
 	}
 
 	createTournamentScreen() {
 		const backToMenu = new Text3D(
-			"ESC TO LEAVE QUEUE",
+			lang.waitingRoom.escToLeave,
 			{ x: -2, y: 1, z: 0 },
 			0xffffff,
 			0.15,
@@ -34,6 +205,7 @@ export class WaitingRoom {
 
 		backToMenu.createText((textMesh) => {
 			textMesh.userData.onClick = backToMenu.onClick;
+			textMesh.userData.textId = 'escToLeave'; // Identificador para actualización de idioma
 			this.scene.add(textMesh);
 			this.buttons.push(textMesh);
 		});
@@ -46,7 +218,7 @@ export class WaitingRoom {
 
 	createMultiplayer() {
 		const waitingText = new Text3D(
-			"Waiting for other player...",
+			lang.waitingRoom.waitingText,
 			{ x: -4, y: 0, z: 0 },
 			0xffffff,
 			0.4,
@@ -55,11 +227,12 @@ export class WaitingRoom {
 		);
 
 		waitingText.createText((textMesh) => {
+			textMesh.userData.textId = 'waitingText'; // Identificador para actualización de idioma
 			this.scene.add(textMesh);
 		});
 
 		const backToMenu = new Text3D(
-			"ESC TO LEAVE QUEUE",
+			lang.waitingRoom.escToLeave,
 			{ x: -2, y: 1, z: 0 },
 			0xffffff,
 			0.2,
@@ -69,6 +242,7 @@ export class WaitingRoom {
 
 		backToMenu.createText((textMesh) => {
 			textMesh.userData.onClick = backToMenu.onClick;
+			textMesh.userData.textId = 'escToLeave'; // Identificador para actualización de idioma
 			this.scene.add(textMesh);
 			this.buttons.push(textMesh);
 		});
@@ -83,6 +257,41 @@ export class WaitingRoom {
 		}
 		else {
 			this.createTournamentScreen();
+		}
+	}
+
+	// Método para manejar cambios de idioma
+	handleLanguageChange() {
+		// Actualizar textos 3D en la escena
+		this.scene.traverse((object) => {
+			if (object.userData && object.userData.textId) {
+				// Solo si el objeto tiene un identificador de texto
+				if (object.userData.textId === 'waitingText') {
+					object.geometry.dispose();
+					new Text3D(
+						lang.waitingRoom.waitingText,
+						object.position,
+						object.material.color.getHex(),
+						0.4,
+						0
+					).updateExistingMesh(object);
+				}
+				else if (object.userData.textId === 'escToLeave') {
+					object.geometry.dispose();
+					new Text3D(
+						lang.waitingRoom.escToLeave,
+						object.position,
+						object.material.color.getHex(),
+						object.position.y === 1 ? 0.15 : 0.2,
+						0
+					).updateExistingMesh(object);
+				}
+			}
+		});
+
+		// Si existe el componente SetNickEl, actualizar sus textos
+		if (this.SetNickEl) {
+			this.SetNickEl.updateLanguage();
 		}
 	}
 
@@ -153,6 +362,7 @@ export class WaitingRoom {
 
     cleanup() {
         window.removeEventListener('keydown', this.boundEscapeHandler);
+        window.removeEventListener('languageChanged', this.languageChangedHandler);
     }
 
     getScene() {
